d883a7d6f22121df1ec284dea74d7f85897672e7
Mary62442 <maria.bu62442@gmail.com>
2018-12-15 20:33:21 +0100

    Added product table creation and relevant tests

--- server/index.js
+++ server/index.js
@@ -39,8 +39,13 @@ console.error('Unable to connect to the database:', err);
 let syncTable =  (tableNametoSync) => {
     return tableNametoSync.sync({force:true}).catch((err) => console.log(err));     
 }
+//To be used in testing:
+exports.syncTable = (tableNametoSync) => {
+    return tableNametoSync.sync({force:true}).catch((err) => console.log(err));     
+}
 
-let createTableValues = (tableName) => {
+//********************People table*******************
+let createPeopleTableValues = (tableName) => {
    return tableName.bulkCreate([
         {
             firstName: 'Jean',
@@ -65,7 +70,6 @@ let createTableValues = (tableName) => {
         }       
     ]).catch((err) => console.log(err));    
 }
-
 exports.sequelizePeopleTable = async () => {
     let Person = sequelize.define( 'person', {
         firstName: {type:Sequelize.STRING, allowNull:false, validate:{len: [2,30]}},
@@ -75,9 +79,74 @@ exports.sequelizePeopleTable = async () => {
         annualEarnings: Sequelize.DOUBLE    
     }); 
     await syncTable(Person);
-    await createTableValues(Person); 
+    await createPeopleTableValues(Person); 
     return Person;
+};
+
+//*******************End of people table************
+
+//****************Product table*************************
+exports.createProductSchema = () => {
+    return sequelize.define( 'product', {
+        sku: {type:Sequelize.STRING, allowNull:false},
+        name: {type: Sequelize.STRING,  allowNull:false,validate:{len: [2,30]}},
+        description: {type:Sequelize.STRING, allowNull:true, validate:{len: [0,500]}},
+        subTitle : {type: Sequelize.STRING, allowNull:true,},
+        unitPrice: {type: Sequelize.DOUBLE,  allowNull:false,},
+        isOnOffer: {type:Sequelize.BOOLEAN,  allowNull:true,},
+        quantityOnOffer: {type:Sequelize.INTEGER,  allowNull:true,},
+        offerPrice: {type:Sequelize.DOUBLE,  allowNull:true,},        
+        category: {type:Sequelize.STRING, allowNull:true,},
+        imageUrl: {type:Sequelize.STRING, allowNull:false,},
+        notes: {type:Sequelize.STRING, allowNull:true, validate:{len: [0, 500]}}
+    })
+};
+
+exports.createProduct = (tableName) => {
+    return tableName.bulkCreate([
+        {
+            sku: "web1",
+            name: "Presentation Website",
+            description: "Website for representation with basic navigation and description.",
+            subTitle : "Basic Website for representation.",
+            unitPrice: 1000,
+            isOnOffer: true,
+            quantityOnOffer: 2,
+            offerPrice: 1800,            
+            category: "IT",
+            imageUrl: "web1.com",
+            notes: ""
+        },
+        { 
+            sku: "web2",
+            name: "Login Register Website",
+            description: "Website with account login.",
+            subTitle : "Website with account login.",
+            unitPrice: 4000,
+            isOnOffer: false,
+            quantityOnOffer: null,
+            offerPrice: null,           
+            category: "IT",
+            imageUrl: "web2.com",
+            notes: ""
+        },
+        { 
+            sku: "web3",
+            name: "High Transactional Website",
+            description: "Website for hight transactional purposes.",
+            subTitle : "Website for hight transactional purposes.",
+            unitPrice: 10000,
+            isOnOffer: false,
+            quantityOnOffer: null,
+            offerPrice: null,           
+            category: "IT",
+            imageUrl: "web3.com",
+            notes: ""
+        }       
+    ]).catch((err) => console.log(err));    
 }
 
 
-
+exports.changeIsOnOfferStatus = (tableName, sku, status) => {
+    return  tableName.update({isOnOffer:status}, {where:{sku:sku}});   
+}
--- test/test-server.js
+++ test/test-server.js
@@ -3,18 +3,17 @@ const expect   = require("chai").expect;
 const Sequelize = require('sequelize');
 const Op = Sequelize.Op;
 
-let person;
-before(async() => {
-    person = await server.sequelizePeopleTable();
-})
-
 describe('generic', () => {
-    it("String has specified length", () => {
+    it("String hello has length equal to 5", () => {
         expect("hello".length).to.equal(5);
     })
 });
 
-describe("Sequelize methods", () => {
+describe("Sequelize people methods", () => {
+    let person;
+    before(async() => {
+        person = await server.sequelizePeopleTable();
+    });
     it("People table to have a count of 3", async () => {        
         let count = await person.count();
         expect(count).to.equal(3);
@@ -33,5 +32,24 @@ describe("Sequelize methods", () => {
         });  
         let count = EarningLess100.length;        
         expect(count).to.equal(2);
+    })
+});
+
+describe("Sequelize products methods", () => {
+    let product;
+    before(async() => {
+        product = await server.createProductSchema();
+        await server.syncTable(product);
+        await server.createProduct(product);  
+    });
+    it("Products table to have a count of 3", async () => {            
+        let count = await product.count();
+        expect(count).to.equal(3);
+    });
+    it("A product with property isOnOffer = true should become false when calling changeIsOnOfferStatus with parameter false", async () => {
+        let skuProductOnOffer = await product.findOne({where:{isOnOffer:true}});   
+        await server.changeIsOnOfferStatus(product, skuProductOnOffer.sku, false);   
+        let isOnOfferAfter = await product.findOne({where:{sku:skuProductOnOffer.sku}}).then(pr => pr.isOnOffer);
+        expect(isOnOfferAfter).to.equal(false);
     })
 });
