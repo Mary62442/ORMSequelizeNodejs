2c7a00cda5958ca29cfc2d4e3e7979b3e7812e3c
Mary62442 <maria.bu62442@gmail.com>
2018-12-15 17:35:55 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('postgres', 'postgres', 'secret', {
+  host: 'localhost',
+  port: 5432,
+  dialect: 'postgres',
+  operatorsAliases: false,
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+  
+});
+
+
+const app = express();
+const allowCrossDomain = function (req, res, next) {      
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, csrf-token, Custom-Auth-Step1, Custom-Auth-Step2, Custom-Auth-Step3, Custom-Auth-Step4');
+    if ('OPTIONS' == req.method) {res.sendStatus(200); }
+    else { next(); }
+}
+
+app.use(allowCrossDomain);
+app.use(bodyParser.json());
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+
+  let Person = sequelize.define( 'person', {
+    firstName: {type:Sequelize.STRING, allowNull:false, validate:{len: [2,30]}},
+    lastName: {type:Sequelize.STRING, allowNull:false, validate:{len: [2,30]}},
+    age: Sequelize.INTEGER,
+    profession: Sequelize.STRING,    
+    annualEarnings: Sequelize.DOUBLE    
+  });
+
+  Person.sync({force:true}).then( () => {
+    Person.create({
+        firstName: 'Jean',
+        lastName: 'Dupont',
+        age:43,    
+        profession : "CEO",
+        annualEarnings:300000.00
+    });
+
+    Person.bulkCreate([
+        { firstName: 'Maria',
+        lastName: 'Burlando',
+        age:21,    
+        profession : "Web Developer",
+        annualEarnings:45000.00 },
+        { firstName: 'Diego',
+        lastName: 'Burlando',
+        age:56,    
+        profession : "Tech Lead",
+        annualEarnings:90000.00},
+        { firstName: 'Monica',
+        lastName: 'Lanzavecchia',
+        age:51,    
+        profession : "Schiava del Vecchio",
+        annualEarnings:0 }
+    ]).then(() => { 
+        return Person.findAll();
+    }).then(people => {
+        console.log(people) 
+    });
+
+    return null;
+  });
+  
